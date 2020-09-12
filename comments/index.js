73fc4ba5798221c4ae1719c25d1dfad01530632f
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-12 11:50:43 +0300

    added cors

--- comments/index.js
+++ comments/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 
 const commentsByPostId = {};
+app.use(cors());
 app.use(bodyParser.json());
 app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,10 @@
 const { randomBytes } = require("crypto");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const posts = {};
