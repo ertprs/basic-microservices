eca06d52dcfe5c023de6668fd830b4149e0726e4
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-12 13:40:22 +0300

    added emitting post event to event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,11 +4,11 @@ const { default: Axios } = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const event = req.body;
-  Axios.post("http://localhost:4000/events", event);
-  Axios.post("http://localhost:4001/events", event);
-  Axios.post("http://localhost:4002/events", event);
+  await Axios.post("http://localhost:4000/events", event);
+  await Axios.post("http://localhost:4001/events", event);
+  await Axios.post("http://localhost:4002/events", event);
   res.send({ status: "OK" });
 });
 
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const { randomBytes } = require("crypto");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(cors());
@@ -10,10 +11,17 @@ app.use(bodyParser.json());
 const posts = {};
 
 app.get("/posts", (req, res) => res.send(posts));
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { id, title };
+  await axios.post("http://localhost:4005", {
+    type: "PostCreated",
+    data: {
+      id,
+      title
+    }
+  });
   res.status(201).send(posts[id]);
 });
 app.listen(4000, () => console.log("listening on port 4000"));
