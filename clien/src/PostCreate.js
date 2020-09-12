064fbd132fc14cd991425e46b0af8dd7d16991f2
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-12 11:40:06 +0300

    added submitting a post

---
+++ clien/src/PostCreate.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+  return (
+    <div>
+      <form
+        onSubmit={async e => {
+          e.preventDefault();
+          await axios.post("http://localhost:4000/posts", { title });
+          setTitle("");
+        }}
+      >
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
