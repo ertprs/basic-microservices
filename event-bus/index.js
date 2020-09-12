f149546dac9b6183bd478be53138bdda90dce2c3
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-12 19:56:46 +0300

    added syncing events on event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,13 +4,17 @@ const { default: Axios } = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
+const events = [];
 app.post("/events", async (req, res) => {
   const event = req.body;
+  events.push(event);
   await Axios.post("http://localhost:4000/events", event);
   await Axios.post("http://localhost:4001/events", event);
   await Axios.post("http://localhost:4002/events", event);
   await Axios.post("http://localhost:4003/events", event);
   res.send({ status: "OK" });
 });
-
+app.get("/events", async (req, res) => {
+  res.send(events);
+});
 app.listen(4005, () => console.log("server started on port 4005"));
