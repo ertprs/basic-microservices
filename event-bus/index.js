1b3bf29b850a9e77b1e1dcdd678c670ef8e15950
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-12 13:22:37 +0300

    added the event bus

---
+++ event-bus/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { default: Axios } = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.post("/events", (req, res) => {
+  const event = req.body;
+  Axios.post("http://localhost:4000/events", event);
+  Axios.post("http://localhost:4001/events", event);
+  Axios.post("http://localhost:4002/events", event);
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => console.log("server started on port 4005"));
