8c5480d248462a0455e6852c5340e3f4f98106a0
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-13 19:24:29 +0300

    connected event bus with other services

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[req.params.id] = comments;
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "CommentCreated",
     data: { id: commentId, content, postId: req.params.id, status: "pending" }
   });
@@ -34,7 +34,7 @@ app.post("/events", async (req, res) => {
     const comments = commentsByPostId[postId];
     const comment = comments.find(com => com.id === id);
     comment.status = data.status;
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentUpdated",
       data: { ...data }
     });
--- query/index.js
+++ query/index.js
@@ -36,7 +36,7 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4002, async () => {
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://event-bus-srv:4005/events");
   res.data.forEach(({ type, data }) => {
     console.log("processing event...", type);
     handleEvent(type, data);
